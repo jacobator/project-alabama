350362f2d9ff0bacba692ea341c895d10e2007c2
Denys Medynskyi <denys.medynskyi@gmail.com>
2016-05-25 21:28:00 +0300

    init rails app

---
+++ app/assets/javascripts/controllers/listsController.js
@@ -0,0 +1,23 @@
+(function() {
+  "use strict";
+  angular
+    .module('projectAlabama')
+      .controller('ListsController', ListsController);
+
+  ListsController.$inject = ['ListsFactory'];
+
+  function ListsController(ListsFactory) {
+    var vm = this;
+    var limit = 10;
+    // vm.getList = getList;
+    setLists();
+
+    function setLists() {
+      ListsFactory.getLists().$promise
+                  .then(function(data) {
+                    vm.lists = data.splice(0, limit);
+                    return vm.lists;
+  				        });
+    }
+  }
+})();
